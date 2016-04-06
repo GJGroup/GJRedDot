926b7b6774d427bf6361aace31e0de69d05eb698
wangyutao <yutaowang@creditease.cn>
2016-04-06 14:28:11 +0800

    1.0

--- GJTabBarItem/UITabBarItem+GJRedDot.h
+++ GJTabBarItem/UITabBarItem+GJRedDot.h
@@ -20,10 +20,13 @@
 @property (nonatomic, assign) CGFloat redDotRadius;
 
 /**
- *  小红点位置偏移，(0,0)默认情况下是:
- *  x:中心偏右10个坐标
- *  y:中心偏上15个坐标
+ *  小红点位置偏移，(0,0)默认情况下是以tabBarButton中心为基准进行偏移:
+ *  x:默认中心偏右10个坐标，以红点原点x坐标为基准，
+ *  也就是说红点frame.origin.x距离tabBarButton中心点的横向坐标距离默认是10
+ *  y:默认中心偏上15个坐标，以红点原点中心点的y坐标为基准，
+ *  也就是说红点frame.center.y距离tabBarButton中心点的纵向坐标距离默认是15
  *  通过offset可以在这基础上偏移
+ *  这么做的目的是当小红点高度变化的时候，不会向下方单方向延伸，而是向上下同时延伸，当长度变化的时候类似badgeVallue一样向右单方向延伸
  */
 @property (nonatomic, assign) CGPoint redDotOffset;
 
--- GJTabBarItem/UITabBarItem+GJRedDot.m
+++ GJTabBarItem/UITabBarItem+GJRedDot.m
@@ -35,6 +35,7 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
     return image;
 }
 
+#pragma mark - GJTabBarButtonDot
 @interface GJTabBarButtonDot : UIImageView
 
 @property (nonatomic, assign) CGFloat radius;
@@ -70,6 +71,9 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
 
 @end
 
+
+#pragma mark - UITabBarItem Category
+
 @interface UITabBarItem ()
 
 @property (nonatomic, readonly) GJTabBarButtonDot *redDotView;
@@ -87,6 +91,17 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
 
 @implementation UITabBarItem (GJRedDot)
 
++ (void)load {
+    Method old = class_getInstanceMethod(self, @selector(setBadgeValue:));
+    Method new = class_getInstanceMethod(self, @selector(gj_setBadgeValue:));
+    method_exchangeImplementations(old, new);
+}
+
+- (void)gj_setBadgeValue:(NSString *)badgeValue {
+    [self gj_setBadgeValue:badgeValue];
+    [self _refreshHiddenState];
+}
+
 - (void)_updateRedDot {
     if (self.customView && !self.customView.superview) {
         [self.nextResponder addSubview:self.customView];
@@ -94,10 +109,23 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
     if (!self.redDotView.superview) {
         [self.nextResponder addSubview:self.redDotView];
     }
-    self.redDotView.hidden = self.customView;
-    self.currentDotView.hidden = !self.isShowRedDot;
-    self.redDotView.center = [self _caculateCenterWithOffset:self.redDotOffset];
-    self.customView.center = [self _caculateCenterWithOffset:self.redDotOffset];
+    
+    [self _refreshHiddenState];
+    [self _refreshDotViewPosition];
+}
+
+- (void)_refreshHiddenState {
+    self.redDotView.hidden = !(!self.customView && self.isShowRedDot && !self.badgeValue);
+    if (self.customView) {
+        self.customView.hidden = !(self.isShowRedDot && !self.badgeValue);
+    }
+}
+
+- (void)_refreshDotViewPosition {
+    if (self.customView) {
+        self.customView.frame = [self _caculateFrameWithBounds:self.customView.bounds];
+    }
+    self.redDotView.frame = [self _caculateFrameWithBounds:self.redDotView.bounds];
 }
 
 - (UIView *)currentDotView {
@@ -127,7 +155,7 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
     BOOL show = [self isShowRedDot];
     if (show != isShowRedDot) {
         objc_setAssociatedObject(self, @selector(isShowRedDot), @(isShowRedDot), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        self.currentDotView.hidden = !isShowRedDot;
+        [self _refreshHiddenState];
     }
 }
 
@@ -138,6 +166,7 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
 
 - (void)setRedDotRadius:(CGFloat)redDotRadius {
     self.redDotView.radius = redDotRadius;
+    [self _refreshDotViewPosition];
 }
 
 //offset
@@ -151,8 +180,7 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
 
 - (void)setRedDotOffset:(CGPoint)redDotOffset {
     objc_setAssociatedObject(self, @selector(redDotOffset),[NSValue valueWithCGPoint:redDotOffset], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    self.redDotView.center = [self _caculateCenterWithOffset:redDotOffset];
-    self.customView.center = [self _caculateCenterWithOffset:redDotOffset];
+    [self _refreshDotViewPosition];
 }
 
 //color
@@ -176,7 +204,6 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
     }
     
     if (customView) {
-        self.redDotView.hidden = YES;
         if (self.nextResponder) {
             [self.nextResponder addSubview:customView];
         }
@@ -184,19 +211,21 @@ static UIImage* gj_createImage(UIColor *color, CGSize size, CGFloat roundSize) {
    
     objc_setAssociatedObject(self, @selector(customView), customView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 
-    self.currentDotView.center = [self _caculateCenterWithOffset:self.redDotOffset];
-    self.currentDotView.hidden = ![self isShowRedDot];
+    [self _refreshHiddenState];
+    [self _refreshDotViewPosition];
 }
 
-- (CGPoint)_caculateCenterWithOffset:(CGPoint)offset {
-    CGRect rect = self.view.frame;
-    CGPoint itemCenter = CGPointMake(rect.origin.x + (rect.size.width / 2),
-                                     rect.origin.y + (rect.size.height / 2));
-    return CGPointMake(GJDefaultOffsetX + offset.x + itemCenter.x, GJDefaultOffsetY + offset.y + itemCenter.y);
+- (CGRect)_caculateFrameWithBounds:(CGRect)bounds {
+    CGRect buttonRect = self.view.frame;
+    CGRect dotRect = bounds;
+    dotRect.origin.x = buttonRect.origin.x + (buttonRect.size.width / 2) + GJDefaultOffsetX + self.redDotOffset.x;
+    dotRect.origin.y = buttonRect.origin.y + (buttonRect.size.height / 2) + GJDefaultOffsetY + self.redDotOffset.y - bounds.size.height / 2;
+    return dotRect;
 }
 
 @end
 
+#pragma mark- UITabBar Category
 @interface UITabBar (GJRedDot)
 
 @end
