953155089cd1a5ab1a1ab24b2e6deeb8d3c8a439
wangyutao <yutaowang@creditease.cn>
2016-05-25 13:53:43 +0800

    beta1

--- GJRedDotDemo/GJRedDotDemo/GJRedDot/UIView+GJRedDot.m
+++ GJRedDotDemo/GJRedDotDemo/GJRedDot/UIView+GJRedDot.m
@@ -130,49 +130,4 @@ static const CGFloat GJDefaultOffsetY = -15;
     self.redDotView.layoutCenterY.constant = y;
 }
 
-//- (UIImageView *)gj_redDotView {
-//    UIImageView *redDot = objc_getAssociatedObject(self, _cmd);
-//    if (!redDot) {
-//        redDot = [[GJRedDotView alloc] initWithFrame:CGRectMake(0, 0, 6, 6)];
-//        [self addSubview:redDot];
-//        objc_setAssociatedObject(self, _cmd, redDot, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-//        redDot.translatesAutoresizingMaskIntoConstraints = NO;
-//
-//        NSLayoutConstraint *layoutX = [NSLayoutConstraint constraintWithItem:self
-//                                                                   attribute:NSLayoutAttributeCenterX
-//                                                                   relatedBy:NSLayoutRelationEqual
-//                                                                      toItem:redDot
-//                                                                   attribute:NSLayoutAttributeCenterX
-//                                                                  multiplier:1
-//                                                                    constant:-25];
-//        NSLayoutConstraint *layoutY = [NSLayoutConstraint constraintWithItem:self
-//                                                                   attribute:NSLayoutAttributeCenterY
-//                                                                   relatedBy:NSLayoutRelationEqual
-//                                                                      toItem:redDot
-//                                                                   attribute:NSLayoutAttributeCenterY
-//                                                                  multiplier:1
-//                                                                    constant:28];
-//        NSLayoutConstraint *layoutH = [NSLayoutConstraint constraintWithItem:redDot
-//                                                                   attribute:NSLayoutAttributeHeight
-//                                                                   relatedBy:NSLayoutRelationEqual
-//                                                                      toItem:nil
-//                                                                   attribute:NSLayoutAttributeHeight
-//                                                                  multiplier:1
-//                                                                    constant:6];
-//        NSLayoutConstraint *layoutW = [NSLayoutConstraint constraintWithItem:redDot
-//                                                                   attribute:NSLayoutAttributeWidth
-//                                                                   relatedBy:NSLayoutRelationEqual
-//                                                                      toItem:nil
-//                                                                   attribute:NSLayoutAttributeWidth
-//                                                                  multiplier:1
-//                                                                    constant:6];
-//        [self addConstraint:layoutX];
-//        [self addConstraint:layoutY];
-//        [self addConstraint:layoutH];
-//        [self addConstraint:layoutW];
-//
-//    }
-//    return redDot;
-//}
-
 @end
