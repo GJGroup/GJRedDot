30e38ee6351f17870e267465b4ddb51b09f02302
wangyutao <yutaowang@creditease.cn>
2016-09-22 15:18:29 +0800

    fixed file place

--- GJRedDot/GJRedDot.h
+++ GJRedDot/GJRedDot.h
@@ -17,8 +17,15 @@
 
 /**
  *  regist your red dot relationship tree from profile,(use NSUserDefault to save data)
+ *  show red dot default.
  */
 + (void)registWithProfile:(NSArray *)profile;
+
+/**
+ *  regist your red dot relationship tree from profile,(use NSUserDefault to save data)
+ *  set show or hide red dot default by 'defaultShow:'.
+ */
++ (void)registWithProfile:(NSArray *)profile defaultShow:(BOOL)show;
 
 /**
  *  regist your red dot relationship tree from profile,
@@ -28,6 +35,34 @@
 + (void)registWithProfile:(NSArray *)profile
                 modelType:(GJRedDotModelType)modelType
            protocolObject:(id<GJRedDotProtocol>)object;
+
+/**
+ *  regist dynamically one by one, after registWithProfile:
+ */
++ (void)registNodeWithKey:(NSString *)key
+                parentKey:(NSString *)parentKey
+              defaultShow:(BOOL)show;
+
+/**
+ *  refresh all node that become show state
+ */
++ (void)resetAllNodesBecomeShown;
+
+/**
+ *  refresh all node that become hide state
+ */
++ (void)resetAllNodesBecomeHidden;
+
+#pragma mark - default setter
+/**
+ *  set default radius of red dot.
+ */
++ (void)setDefaultRadius:(CGFloat)radius;
+
+/**
+ *  set default color of red dot.
+ */
++ (void)setDefaultColor:(UIColor *)color;
 
 
 @end
--- GJRedDot/GJRedDot.m
+++ GJRedDot/GJRedDot.m
@@ -7,23 +7,61 @@
 //
 
 #import "GJRedDot.h"
+#import "GJRedDotView.h"
 
 @implementation GJRedDot
 
+//default show
 + (void)registWithProfile:(NSArray *)profile {
+    [self registWithProfile:profile defaultShow:YES];
+}
+
+//can set show or hide
++ (void)registWithProfile:(NSArray *)profile defaultShow:(BOOL)show {
     NSAssert(profile.count, @"GJRedDot: You can't regist an empty profiles");
-    [[GJRedDotManager sharedManager] registWithProfile:profile];
+    [[GJRedDotManager sharedManager] registWithProfile:profile defaultShow:show];
 }
 
 + (void)registWithProfile:(NSArray *)profile
                 modelType:(GJRedDotModelType)modelType
            protocolObject:(id<GJRedDotProtocol>)object {
+    NSAssert(profile.count, @"GJRedDot: You can't regist an empty profiles");
     if ((modelType == GJRedDotModelCustom && !object) ||
         modelType == GJRedDotModelUserDefault) {
         [self registWithProfile:profile];
         return;
     }
-    
-    
+    [[GJRedDotManager sharedManager] registWithProfile:profile
+                                             modelType:modelType
+                                        protocolObject:object];
 }
+
++ (void)registNodeWithKey:(NSString *)key
+                parentKey:(NSString *)parentKey
+              defaultShow:(BOOL)show{
+    
+    [[GJRedDotManager sharedManager] registNodeWithKey:key
+                                             parentKey:parentKey
+                                           defaultShow:show];
+}
+
++ (void)resetAllNodesBecomeShown{
+    [[GJRedDotManager sharedManager] resetAllShown];
+}
+
++ (void)resetAllNodesBecomeHidden{
+    [[GJRedDotManager sharedManager] resetAllHidden];
+}
+
+
+#pragma mark- default setter
+
++ (void)setDefaultRadius:(CGFloat)radius {
+    [GJRedDotView setDefaultRadius:radius];
+}
+
++ (void)setDefaultColor:(UIColor *)color {
+    [GJRedDotView setDefaultColor:color];
+}
+
 @end
